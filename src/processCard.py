496ed3bfa083b90c15ab4a2d6f1c80ee17e10dbe
Tyler Vo <tylervo02@yahoo.com>
2026-04-27 23:32:11 -0700

    Endpoint now predicts chances of card being fake. Will need further work on model.

--- src/processCard.py
+++ src/processCard.py
@@ -20,17 +20,26 @@ def root():
 @router.post("/predict", summary = "Predict Card Class")
 # I will need to figure out if it is even a valid card to be processed in the first place. Right now, that functionality isn't really supported.
 async def predictCard(file: UploadFile = File(...)):
-    if not file.filename.lower().endswith((".png", ".jpg", "jpeg")):
+    if not file.filename.lower().endswith((".png", ".jpg", ".jpeg")):
         raise HTTPException(
             status_code = 400,
             detail = "Invalid image format. Only PNG and JPG are allowed.",
         )
     try:
         rawImage = await file.read()
-        image = Image.open(io.BytesIO(rawImage).convert("RGB"))
+        image = Image.open(io.BytesIO(rawImage)).convert("RGB")
     except Exception:
         raise HTTPException(status_code=400, detail="Error processing images.")
     
     #preprocess the image 
-    inputToTensor = tf.cast(tf.image.resize(image, (224, 224)), tf.float32) / 255.0
-    return JSONResponse({"predicted_class": classNames[int(tf.argmax(model.predict(inputToTensor)[0]))]})
+    inputToTensor = tf.convert_to_tensor(image)
+    inputToTensor = tf.image.resize(inputToTensor, (224, 224))
+    inputToTensor = tf.cast(inputToTensor, tf.float32) / 255.0
+    inputToTensor = tf.expand_dims(inputToTensor, axis=0)
+
+    # return JSONResponse({"predicted_class": classNames[int(tf.argmax(model.predict(inputToTensor)[0]))]})
+
+    pred = model.predict(inputToTensor)[0]
+    return JSONResponse({
+        "fakechance": float(pred[0])
+    })
