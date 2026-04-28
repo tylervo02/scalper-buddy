d9f55c22fbf377089fe939d3f2904a912cc4fe30
Tyler Vo <tylervo02@yahoo.com>
2026-04-27 22:55:28 -0700

    Finish setting up backend router, will work on frontend next.

--- src/processCard.py
+++ src/processCard.py
@@ -4,15 +4,16 @@ from PIL import Image
 import io
 import tensorflow as tf
 from tensorflow import keras
+import uvicorn
 
 router = APIRouter()
-model = keras.model.load_model("./fakeCardCNN.keras") #figure out where I put .keras file
+model = keras.models.load_model("./fakeCardCNN.keras") #figure out where I put .keras file
 classNames = [
     "real",
     "fake"
 ]
 
-@app.post("/predict", summary = "Predict Card Class")
+@router.post("/predict", summary = "Predict Card Class")
 # I will need to figure out if it is even a valid card to be processed in the first place. Right now, that functionality isn't really supported.
 async def predictCard(file: UploadFile = File(...)):
     if not file.filename.lower().endswith((".png", ".jpg", "jpeg")):
@@ -29,4 +30,6 @@ async def predictCard(file: UploadFile = File(...)):
     #preprocess the image 
     inputToTensor = tf.cast(tf.image.resize(image, (224, 224)), tf.float32) / 255.0
     return JSONResponse({"predicted_class": classNames[model.predict(inputToTensor)]})
-    
+
+if __name__ == "__main__":
+    uvicorn.run(router, host="127.0.0.1", port=8080)
